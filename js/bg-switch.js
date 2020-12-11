695ae80362577d7c87de5d8d87ad8e6f20f0d188
Aurimas <aurcin@gmail.com>
2020-12-11 22:58:25 +0200

    optimized js to manipulate on scroll only once on passing breakpoint instead on every scroll

--- js/bg-switch.js
+++ js/bg-switch.js
@@ -1,12 +1,27 @@
 const bg = document.querySelector('.bg-image-1');
+let showSecond = false;
 
 window.addEventListener('scroll', (event) => {
   let scroll = this.scrollY;
   if (scroll > 1000) {
+    if (!showSecond) {
+      showSecond = true;
+      switchBgImages();
+    }
+  } else {
+    if (showSecond) {
+      showSecond = false;
+      switchBgImages();
+    }
+  }
+});
+
+const switchBgImages = () => {
+  if (bg.classList.contains('layer-far')) {
     bg.classList.add('layer-back');
     bg.classList.remove('layer-far');
   } else {
     bg.classList.add('layer-far');
     bg.classList.remove('layer-back');
   }
-});
+};
